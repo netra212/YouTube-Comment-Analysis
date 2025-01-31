1d68ee6fa1a95fcdbf0ca09c08b9a99332b83ed1
netra217 <netra200021kcbdr@gmail.com>
2025-01-31 14:30:30 +0000

    adding data_ingestion, dvc and params

---
+++ youtube-comment-sentiment-analysis/src/models/model_building.py
@@ -0,0 +1,6 @@
+# In this stage
+# First fetch the data from interm i..e, train and test.
+# Vectorizer the data with TF-IDF and trigram, and max_features.... 1000
+# Builds the yaml.
+    # LightGBM.
+    # n_estimators.
